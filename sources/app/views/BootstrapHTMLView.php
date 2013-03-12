1402437e5958e9949cbd72e36e54e5d4ecc6fb1b
luis <luis@luis>
2013-03-12 15:41:19 -0300

    Subo vista de Bootstrap

---
+++ sources/app/views/BootstrapHTMLView.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once ("app/views/HTMLView.php");
+
+class BootstrapHTMLView extends HTMLView
+{
+    protected function build() 
+    {
+        $this->headTag->add (new Tag("meta", array("name"=>"viewport", "content"=>"width=device-width, initial-scale=1.0")));
+        $this->headTag->add (new Tag("link", array("href"=>"assets/bootstrap/css/bootstrap.min.css", "rel"=>"stylesheet", "media"=>"screen")));
+        $this->addScriptFile("http://code.jquery.com/jquery.js");
+        $this->addScriptFile("assets/bootstrap/js/bootstrap.min.js");
+    }
+}
+
+?>
