ca7b68929d62a5ae9eda6d0a9e985877afac212e
luis <luis@luis>
2013-03-12 16:07:20 -0300

    Modifico vista de Bootstrap para que utilize jquery obfuscado

--- sources/app/views/BootstrapHTMLView.php
+++ sources/app/views/BootstrapHTMLView.php
@@ -8,7 +8,7 @@ class BootstrapHTMLView extends HTMLView
     {
         $this->headTag->add (new Tag("meta", array("name"=>"viewport", "content"=>"width=device-width, initial-scale=1.0")));
         $this->headTag->add (new Tag("link", array("href"=>"assets/bootstrap/css/bootstrap.min.css", "rel"=>"stylesheet", "media"=>"screen")));
-        $this->addScriptFile("http://code.jquery.com/jquery.js");
+        $this->addScriptFile("http://code.jquery.com/jquery.min.js");
         $this->addScriptFile("assets/bootstrap/js/bootstrap.min.js");
     }
 }
