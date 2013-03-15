b0dd3af5de229ad90432e479607f7ab9d0a8b47f
luis <luis@luis-VGN-N330FH>
2013-03-14 23:08:06 -0300

    Correcciones de código en la vista de Bootstrap

--- sources/app/views/BootstrapHTMLView.php
+++ sources/app/views/BootstrapHTMLView.php
@@ -2,27 +2,27 @@
 
 require_once ("app/views/HTMLView.php");
 
-class BootstrapHTMLView extends HTMLView
+abstract class BootstrapHTMLView extends HTMLView
 {
     private $responsive = true;
     
-    public function setResponsive ($responsive)
+    public final function setResponsive ($responsive)
     {
         $this->responsive = $responsive;
     }
     
-    protected function build() 
+    protected final function build() 
     {
         $this->headTag->add (new Tag("meta", array("name"=>"viewport", "content"=>"width=device-width, initial-scale=1.0")));
         $this->addScriptFile("http://code.jquery.com/jquery.min.js");
         $this->addScriptFile("assets/bootstrap/js/bootstrap.min.js");
         $this->addStyleFile("assets/bootstrap/css/bootstrap.min.css");
-        $this->addStyles ();
+        $this->buildView ();
         if ($this->responsive)
             $this->addStyleFile("assets/bootstrap/css/bootstrap-responsive.min.css");    
     }
     
-    protected function addStyles () {}
+    protected abstract function buildView ();
 }
 
 ?>
