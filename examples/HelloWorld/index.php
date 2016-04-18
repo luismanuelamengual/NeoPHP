d4b43b40b4c5cfe87989096a07195791ecdc54f1
Luis <luis.amengual@sitrack.com>
2016-04-18 17:15:30 -0300

    Borro archivos de NeoPHP 2.0.0

--- examples/HelloWorld/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once ("/home/luis/git/NeoPHP2/sources/bootstrap.php");
-NeoPHP\ClassLoader::getInstance()->addIncludePath(__DIR__."/src");
-com\neophp\NeoApplication::getInstance()->start();
-
-?>
