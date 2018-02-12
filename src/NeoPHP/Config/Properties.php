119bc8d574fa85245597df8806a6ae3b5c67d534
luis <luismanuelamengual@gmail.com>
2018-02-12 14:11:56 -0300

    Primera version de las propiedades de la aplicación

---
+++ src/NeoPHP/Config/Properties.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace NeoPHP\Config;
+
+use NeoPHP\Core\Application;
+
+/**
+ * Class Properties
+ * @package NeoPHP\Config
+ */
+abstract class Properties {
+
+    private static $properties = [];
+
+    /**
+     * @param $key
+     * @param null $defaultValue
+     * @return array|mixed|null
+     */
+    public static function get($key, $defaultValue = null) {
+        $keyTokens = explode(".", $key);
+        if (!isset(self::$properties[$keyTokens[0]])) {
+            self::loadPropertiesModule($keyTokens[0]);
+        }
+        $propertyValue = self::$properties;
+        foreach ($keyTokens as $keyToken) {
+            if (isset($propertyValue[$keyToken])) {
+                $propertyValue = $propertyValue[$keyToken];
+            }
+            else {
+                $propertyValue = null;
+                break;
+            }
+        }
+        return $propertyValue == null? $defaultValue : $propertyValue;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public static function set($key, $value) {
+        $keyTokens = explode(".", $key);
+        $propertyKey = &self::$properties;
+        foreach ($keyTokens as $keyToken) {
+            if (!isset($propertyKey[$keyToken])) {
+                $propertyKey[$keyToken] = [];
+            }
+            $propertyKey = &$propertyKey[$keyToken];
+        }
+        $propertyKey = $value;
+    }
+
+    /**
+     * @param $moduleName
+     */
+    private static function loadPropertiesModule($moduleName) {
+        self::$properties[$moduleName] = @include_once(Application::getConfigPath() . DIRECTORY_SEPARATOR . $moduleName . ".php");
+    }
+}
