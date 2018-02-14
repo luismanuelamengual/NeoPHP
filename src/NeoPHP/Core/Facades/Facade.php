e040d5715d0bae9ca2ff00d02129f92b3e9acafc
luis <luismanuelamengual@gmail.com>
2018-02-14 10:25:35 -0300

    Agrego comentarios

--- src/NeoPHP/Core/Facades/Facade.php
+++ src/NeoPHP/Core/Facades/Facade.php
@@ -5,10 +5,17 @@ namespace NeoPHP\Core\Facades;
 use NeoPHP\Core\Application;
 use RuntimeException;
 
+/**
+ * Class Facade
+ * @package NeoPHP\Core\Facades
+ */
 abstract class Facade {
 
     private static $instance;
 
+    /**
+     * @return mixed|null|void
+     */
     private static function getInstance () {
         if (!isset(self::$instance)) {
             self::$instance = self::createInstance();
@@ -16,14 +23,23 @@ abstract class Facade {
         return self::$instance;
     }
 
+    /**
+     * Get the interface class of the facade
+     */
     protected static function getFacadeClass () {
         throw new RuntimeException('Facade does not implement getFacadeClass method.');
     }
 
+    /**
+     * Get the instance implementation of the facade
+     */
     protected static function createDefaultFacadeImplementation (){
         throw new RuntimeException('Facade does not implement createDefaultFacadeImplementation method.');
     }
 
+    /**
+     * @return mixed|null|void
+     */
     private static function createInstance () {
         $instance = null;
         $facadeClass = static::getFacadeClass();
@@ -42,6 +58,11 @@ abstract class Facade {
         return $instance;
     }
 
+    /**
+     * @param $method
+     * @param $args
+     * @return mixed
+     */
     public static function __callStatic($method, $args) {
         return self::getInstance()->$method(...$args);
     }
