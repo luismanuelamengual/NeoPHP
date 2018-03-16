439380f9068b6a76cf8c780ce6bc278a94c6a5dc
Luis Amengual <luis.amengual@sitrack.com>
2018-03-16 09:23:40 -0300

    Corrección de sesión

--- src/NeoPHP/Http/Session.php
+++ src/NeoPHP/Http/Session.php
@@ -71,11 +71,17 @@ final class Session {
     }
 
     /**
-     *
+     * Removes all data associated with the session
+     */
+    public function clear () {
+        $_SESSION = [];
+    }
+
+    /**
+     * Destroys the session and removes all data
      */
     public function destroy() {
-        $_SESSION = [];
-
+        $this->clear();
         if (ini_get("session.use_cookies")) {
             $params = session_get_cookie_params();
             setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
