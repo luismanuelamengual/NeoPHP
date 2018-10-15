da7c5831974b37dd4b0b170d9aa64e61404d819b
luis <luismanuelamengual@gmail.com>
2018-10-15 14:22:24 -0300

    Correcciones de código

--- src/NeoPHP/Database/DB.php
+++ src/NeoPHP/Database/DB.php
@@ -3,6 +3,7 @@
 namespace NeoPHP\Database;
 
 use NeoPHP\Database\Builder\PostgresQueryBuilder;
+use NeoPHP\Database\Query\Query;
 use PDO;
 use RuntimeException;
 
@@ -15,10 +16,11 @@ abstract class DB {
     private static $connections = [];
 
     /**
-     * @param null $connectionName
-     * @return Connection
+     * Returns a connection
+     * @param string|null $connectionName name of a connection
+     * @return Connection database connection
      */
-    public static function connection($connectionName=null): Connection {
+    public static function connection(?string $connectionName=null): Connection {
         if ($connectionName == null) {
             $connectionName = get_property("database.default");
             if ($connectionName == null) {
@@ -62,7 +64,8 @@ abstract class DB {
     }
 
     /**
-     * @param $table
+     * Returns a connection query to a certain table on the default connection
+     * @param string $table table to make a query
      * @return ConnectionTable
      */
     public static function table($table): ConnectionTable {
@@ -70,8 +73,9 @@ abstract class DB {
     }
 
     /**
-     * @param $sql
-     * @param array $bindings
+     * Execute an query on the default connection
+     * @param Query|string $sql query to be executed
+     * @param array $bindings sql bindings
      * @return bool|int
      */
     public static function exec($sql, array $bindings = []) {
@@ -79,39 +83,44 @@ abstract class DB {
     }
 
     /**
-     * @param $sql
-     * @param array $bindings
-     * @return array
+     * Executes a query sql on the defaul connection
+     * @param Query|string $sql query to be executed
+     * @param array $bindings sql bindings
+     * @return array results
      */
     public static function query($sql, array $bindings = []) {
         return self::connection()->query($sql, $bindings);
     }
 
     /**
-     * @param null $readOnly
+     * Set the default connection to be read only
+     * @param bool|null $readOnly read only
      * @return bool|mixed
      */
-    public static function readOnly($readOnly=null) {
+    public static function readOnly(?bool $readOnly=null) {
         return self::connection()->readOnly($readOnly);
     }
 
     /**
+     * Set the default connection logging enabled
+     * @param bool|null $logEnabled log enabled
      * @return bool|mixed
      */
-    public static function logEnabled($logEnabled=null) {
+    public static function logEnabled(?bool $logEnabled=null) {
         return self::connection()->logEnabled($logEnabled);
     }
 
     /**
-     * @param null $debugEnabled
+     * Set the default connection debug mode
+     * @param bool|null $debugEnabled debug enabled
      * @return bool|mixed
      */
-    public static function debugEnabled($debugEnabled=null) {
+    public static function debugEnabled(?bool $debugEnabled=null) {
         return self::connection()->debugEnabled($debugEnabled);
     }
 
     /**
-     * Registra un nuevo listener de sentencias sql
+     * Listens for sql queries
      * @param callable $listener
      */
     public static function listen(callable $listener) {
@@ -119,7 +128,8 @@ abstract class DB {
     }
 
     /**
-     * @param $name
+     * Calls static methods for the default connection
+     * @param string $name name of method
      * @param $arguments
      * @return mixed
      */
