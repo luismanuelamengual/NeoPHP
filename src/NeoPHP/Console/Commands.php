e89d2c4cb6c6bf582ed3319fc84014ff68ee4039
Luis Amengual <luis.amengual@sitrack.com>
2019-08-14 11:51:25 -0300

    Subo pack de cambios 2

--- src/NeoPHP/Console/Commands.php
+++ src/NeoPHP/Console/Commands.php
@@ -19,7 +19,7 @@ abstract class Commands {
      * @param string $commandName name of the command
      * @param mixed $action action to be executed
      */
-    public static function register(string $commandName, $action) {
+    public static function register($commandName, $action) {
         self::$commands[$commandName] = $action;
     }
 
@@ -28,48 +28,42 @@ abstract class Commands {
      * @throws \Exception
      */
     public static function handleCommand() {
-        try {
-            global $argv;
-            $tokens = array_slice($argv, 1);
+        global $argv;
+        $tokens = array_slice($argv, 1);
 
-            if (empty($tokens)) {
-                throw new RuntimeException("Command name is required !!");
-            }
-
-            $commandName = (string)$tokens[0];
-            $commandTokens = array_slice($tokens, 1);
-
-            $commandParameters = [];
-            $currentCommandName = null;
-
-            for ($i = 0; $i < sizeof($commandTokens); $i++) {
-                $commandToken = $commandTokens[$i];
-
-                if (substr($commandToken, 0, 2) == '--') {
-                    if ($currentCommandName != null) {
-                        $commandParameters[$currentCommandName] = true;
-                    }
-                    $currentCommandName = substr($commandToken, 2);
-                }
-                else {
-                    if ($currentCommandName == null) {
-                        throw new RuntimeException("Invalid argument \"$commandToken\". Arguments must start with \"--\"");
-                    }
-                    $commandParameters[$currentCommandName] = $commandToken;
-                    $currentCommandName = null;
-                }
-            }
-
-            if ($currentCommandName != null) {
-                $commandParameters[$currentCommandName] = true;
-            }
-
-            self::executeCommand($commandName, $commandParameters);
+        if (empty($tokens)) {
+            throw new RuntimeException("Command name is required !!");
         }
-        catch (CommandNotFoundException $exception) {
-            get_logger()->warning($exception->getMessage());
-            echo $exception->getMessage() . "\n";
+
+        $commandName = (string)$tokens[0];
+        $commandTokens = array_slice($tokens, 1);
+
+        $commandParameters = [];
+        $currentCommandName = null;
+
+        for ($i = 0; $i < sizeof($commandTokens); $i++) {
+            $commandToken = $commandTokens[$i];
+
+            if (substr($commandToken, 0, 2) == '--') {
+                if ($currentCommandName != null) {
+                    $commandParameters[$currentCommandName] = true;
+                }
+                $currentCommandName = substr($commandToken, 2);
+            }
+            else {
+                if ($currentCommandName == null) {
+                    throw new RuntimeException("Invalid argument \"$commandToken\". Arguments must start with \"--\"");
+                }
+                $commandParameters[$currentCommandName] = $commandToken;
+                $currentCommandName = null;
+            }
         }
+
+        if ($currentCommandName != null) {
+            $commandParameters[$currentCommandName] = true;
+        }
+
+        self::executeCommand($commandName, $commandParameters);
     }
 
     /**
@@ -84,25 +78,20 @@ abstract class Commands {
             $commandAction = self::$commands[$commandName];
         }
         else {
-            $commandsBaseNamespace = get_property("cli.commands_base_namespace");
-            if (!empty($commandsBaseNamespace)) {
-                $commandAction = $commandsBaseNamespace;
-                $commandTokens = explode(".", $commandName);
-                $commandTokensSize = sizeof($commandTokens);
-                if ($commandTokensSize > 1) {
-                    for ($i = 0; $i < $commandTokensSize - 1; $i++) {
-                        $commandToken = $commandTokens[$i];
-                        $commandAction .= '\\';
-                        $commandAction .= str_replace(' ', '', ucwords(str_replace('_', ' ', $commandToken)));
-                    }
+            $commandsBaseNamespace = get_property("cli.commands_base_namespace", "NeoPHP\\Commands");
+            $commandAction = $commandsBaseNamespace;
+            $commandTokens = explode(".", $commandName);
+            $commandTokensSize = sizeof($commandTokens);
+            if ($commandTokensSize > 1) {
+                for ($i = 0; $i < $commandTokensSize - 1; $i++) {
+                    $commandToken = $commandTokens[$i];
+                    $commandAction .= '\\';
+                    $commandAction .= str_replace(' ', '', ucwords(str_replace('_', ' ', $commandToken)));
                 }
-                $commandAction .= '\\';
-                $commandAction .= str_replace(' ', '', ucwords(str_replace('_', ' ', $commandTokens[$commandTokensSize - 1])));
-                $commandAction .= get_property("cli.commands_suffix", "Command");
             }
-            else {
-                throw new CommandNotFoundException("Command \"$commandName\" not found !!");
-            }
+            $commandAction .= '\\';
+            $commandAction .= str_replace(' ', '', ucwords(str_replace('_', ' ', $commandTokens[$commandTokensSize - 1])));
+            $commandAction .= get_property("cli.commands_suffix", "Command");
         }
 
         $result = null;
@@ -114,10 +103,10 @@ abstract class Commands {
         }
         else  {
             try {
-                get_app()->execute($commandAction, $commandParameters);
+                get_app()->execute($commandName, $commandParameters);
             }
             catch (ActionNotFoundException $ex) {
-                throw new CommandNotFoundException("Command \"$commandName\" not found !!. Command class \"$commandAction\" not found !! ", 0, $ex);
+                throw new CommandNotFoundException("Action \"$commandName\" not found !!.", 0, $ex);
             }
         }
         return $result;
